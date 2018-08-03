2430776434918a86618e9ff25e23063644d8f85e
Sanuja-Laptop <sanujac@live.com>
2018-08-03 22:48:41 +0530

    Added socket.io with connect and disconnect handlers

--- server/server.js
+++ server/server.js
@@ -1,23 +1,31 @@
 require('./config/config')
 
-const express = require('express')
-const bodyParser = require('body-parser')
-
 const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname, '../public')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketIO(server)
 
 app.use(express.static(publicPath))
-// app.use(bodyParser.json())
+
+// Registers an event listener
+// Lets you listen for a new connection
+io.on('connection', (socket)=>{
+    console.log('New user connected')
+
+    socket.on('disconnect', (socket)=>{
+        console.log('User was disconnected')
+    })
+    
+})
 
 
-// app.get('/', (req, res) => {
-//     res.send(publicPath + '/index.htm')
-// })
-
-app.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
     console.log(`Server is up on port ${process.env.PORT}`)
 })
 
